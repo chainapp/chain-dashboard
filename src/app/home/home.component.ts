eaa030150b2a8f0a00f234eb56a0c7d7e5f84633
Louis Stockreisser <louis.stockreisser@gmail.com>
2017-01-05 13:59:29 +0100

    [LST] mails bug fix + invite more first version

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
 import {ChainService} from '../services/chain.service';
 import {AuthService} from '../services/auth.service';
-
+import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
+import {ToasterContainerComponent, ToasterService} from 'angular2-toaster/angular2-toaster';
 
 @Component({
   selector: 'home',
@@ -11,11 +12,20 @@ import {AuthService} from '../services/auth.service';
 })
 export class HomeComponent {
 	public chains:any = [];
+	private selectedChain: any = {};
+
 	@Input() 
 	model: any;
+	@ViewChild('inviteModal')
+  	inviteModal: ModalComponent;
+  	private toasterService: ToasterService;
+
+  	sms: any = "Copy/Paste phone numbers (format 336/7XXXXXXXX) separated by commas";
+	mails: any = "Copy/Paste emails separated by commas";
 
 	
-	constructor(private router: Router, private chain: ChainService, private authService: AuthService) {
+	constructor(private router: Router, private chain: ChainService, toasterService: ToasterService, private authService: AuthService) {
+		 this.toasterService = toasterService;
 	}
 
 	ngOnInit() {
@@ -41,5 +51,46 @@ export class HomeComponent {
         console.log("received "+searchstring)
     }
 
+    inviteMore(chain: any){
+    	this.selectedChain = chain;
+	 	this.inviteModal.open();
+	 }
+
+	 confirmInviteMore(){
+	 	this.chain.invite(this.selectedChain._id,(this.sms ? this.sms.replace(/ /g, "").split(',') : []),(this.mails ? this.mails.replace(/ /g, "").split(',') : []),this.authService.token()).subscribe(
+	          	res => {
+		          console.log(res);
+		          this.inviteModal.close();
+		          this.toasterService.pop('success', 'Invites sent', 'Your invites have been sent !');
+		          this.selectedChain = {};
+		 			this.sms = "Copy/Paste phone numbers (format 336/7XXXXXXXX) separated by commas";
+					this.mails = "Copy/Paste emails separated by commas";
+		        },
+		        err => {
+		        	this.inviteModal.close();
+		        	//var message = JSON.parse(err._body).message
+		            this.toasterService.pop('error', 'Error', 'Error occured during invitations. Please try again.');
+		            this.selectedChain = {};
+				 	this.sms = "Copy/Paste phone numbers (format 336/7XXXXXXXX) separated by commas";
+					this.mails = "Copy/Paste emails separated by commas";
+		        }
+	          )
+	 }
+
+	 refuseInviteMore(){
+	 	this.inviteModal.close();
+	 	this.selectedChain = {};
+	 	this.sms = "Copy/Paste phone numbers (format 336/7XXXXXXXX) separated by commas";
+		this.mails = "Copy/Paste emails separated by commas";
+	 }
+
+
+	 emptySMS() {
+  		this.sms = null;
+	  }
+
+	  emptyMails() {
+  		this.mails = null;
+	  }
 	
 }
